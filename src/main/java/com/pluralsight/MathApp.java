caab0e06754d5ac30e263d2e86d0d0540dd812a6
mili Arsema <meleymm88@gmail.com>
2026-04-14 04:05:15 -0500

    Completed question 1 of step-1

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+
+        //Q-1
+
+        double bobSalary = 65000;
+        double garySalary = 67000;
+        double highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is: " + highestSalary);
+
+    }
 }
