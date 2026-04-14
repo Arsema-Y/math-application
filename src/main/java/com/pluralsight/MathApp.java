370025114139bec652c00384abafe862e96c4e93
mili Arsema <meleymm88@gmail.com>
2026-04-14 05:10:27 -0500

    Completed question 2 of step-1

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -10,11 +10,21 @@ public class MathApp {
         // ex: System.out.println("The answer is " + answer);
 
         //Q-1
-
+        //Determining the highest salary using Math.max
         double bobSalary = 65000;
         double garySalary = 67000;
         double highestSalary = Math.max(bobSalary, garySalary);
+        //display result
         System.out.println("The highest salary is: " + highestSalary);
+
+        //Q-2
+        //Determining the lowest price using Math.min
+        double carPrice = 34999.89;
+        double truckPrice = 41999.99;
+        double lowestPrice = Math.min(carPrice, truckPrice);
+        //display result
+        System.out.println("The lowest price is: " + lowestPrice);
+
 
     }
 }
